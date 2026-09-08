075cd47c9c5f029e35a588f06fbb0d448899a84b
wunlight <gluigi132@gmail.com>
2026-09-08 14:09:51 +0700

    feat: added cors config

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,14 @@
+import cors from "cors";
 import express from "express";
 import router from "./routes/index.js";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  }),
+);
 
 app.use(express.json());
 
